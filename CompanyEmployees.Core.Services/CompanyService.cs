f1ae39f45d56c31dfc0db24cd226e261e442d213
Vladimir Cortés Arévalo <vcortes@sena.edu.co>
2026-05-25 11:38:35 -0500

    feat(backend): refactor Layer Service. AddMappingProfile

--- CompanyEmployees.Core.Services/CompanyService.cs
+++ CompanyEmployees.Core.Services/CompanyService.cs
@@ -1,4 +1,5 @@
-﻿using CompanyEmployees.Core.Domain.Entities;
+﻿using AutoMapper;
+using CompanyEmployees.Core.Domain.Entities;
 using CompanyEmployees.Core.Domain.Repositories;
 using CompanyEmployees.Core.Services.Abstractions;
 using LoggingService;
@@ -10,11 +11,13 @@ internal sealed class CompanyService : ICompanyService
 {
     private readonly IRepositoryManager _repository;
     private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
 
-    public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+    public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
     {
         _repository = repository;
         _logger = logger;
+        _mapper = mapper;
     }
 
     public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
@@ -22,9 +25,14 @@ internal sealed class CompanyService : ICompanyService
         try
         {
             var companies = _repository.Company.GetAllCompanies(trackChanges);
-            var compiesDto = companies.Select(c => new 
-                CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country)))
-                .ToList();
+            
+            //var compiesDto = companies.Select(c => new 
+            //    CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country)))
+            //    .ToList();
+
+            var compiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);   // AutoMapper approach
+
+
             return compiesDto;
         }
         catch (Exception ex)
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -10,6 +10,8 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 // Add services to the container.
 
